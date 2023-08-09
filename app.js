41d0e6e17ea100e4b36615c8f9d125f930a5f0a4
brickphone <mattis.hutt@gmail.com>
2023-08-09 20:50:05 +0200

    working team names

--- app.js
+++ app.js
@@ -6,37 +6,60 @@ const app = express()
 
 const PORT = process.env.port || 3000;
 
-const website = "https://www.livescore.com/en/"
+const website = "https://www.livescore.bz/"
+
+console.log("Before scraping process")
 
 async function scrapeData() {
   try {
-    console.log("Initiating scraping process")
-    axios(website).then((res) => {
-      const data = res.data;
+      console.log("Initiating scraping process")
+
+      const response = await axios.get(website, {
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        }
+      })
+      const data = response.data;
       const $ = cheerio.load(data);
 
       let content = [];
 
-      $(".np", data).each(function () {
-        const title = $(this).text();
-        const url = $(this).find("a").attr("href");
-
+      $("a.m.live.meven").each(function () {
+        const matchTime = $(this).find("st").text().trim();
+        const homeTeam = $(this).find("t1").eq(0).text().trim();
+        const awayTeam = $(this).find("t2").eq(1).text().trim();
+  
         content.push({
-          match,
-          teams,
+          matchTime: matchTime,
+          homeTeam: homeTeam,
+          awayTeam: awayTeam,
         });
-
-        console.log("Scraping process completed.")
-
-        app.get("/", (req, res) => {
-          res.json(content);
-        })
       });
-    });
+
+    console.log(content)
+    console.log("Scraping process completed.")
+
   } catch (error) {
-    console.log(error, error.message)
+    console.log(error, error.message,)
+    return [];
   }
 }
+
+// node router
+app.get("/", async (req, res) => {
+  try {
+    const scrapedData = await scrapeData();
+
+    console.log(scrapedData)
+
+    res.json(scrapeData);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while fetching data." });
+  }
+});
+
+scrapeData();
+
 
 app.listen(PORT, () => {
   console.log(`server is running on PORT:${PORT}`);
