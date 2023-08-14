61a2e84784097a7974e612839a34cbc2aa24e3a8
brickphone <mattis.hutt@gmail.com>
2023-08-14 22:31:08 +0200

    :D

--- Scraper/scraper.js
+++ Scraper/scraper.js
@@ -1,5 +1,11 @@
 const puppeteer = require("puppeteer");
 
 async function scrapeData() {
-  const
+  const browser = await puppeteer.launch( {headless: false} );
+  const page = await browser.newPage("")
+  await page.setViewport({ width: 1000, height: 926 });
+
+  await page.goto("https://www.livescore.com/en/")
 }
+
+scrapeData();
