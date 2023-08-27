0f4e4eecdc8ef029b8d8e1b407bf259e27b63c2f
brickphone <mattis.hutt@gmail.com>
2023-08-27 15:44:14 +0200

    Revert "added stuff"
    This reverts commit a74736040c0e360c49505889a2b77dcc48652351.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const express = require("express");
-const mongoose = require("mongoose");
 const scrapeData = require("./Scraper/scraper");
 const routes = require("./routes/routes");
 
@@ -23,23 +22,13 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
-
-app.get("/status", (request, response) => {
-  const status = {
-    "Status":  "Running"
-  };
-
-  response.send(status)
-});
-
-
 app.get("/api/data", async(req, res) => {
   try {
     const page = await openPage();
     const dataScraped = await scrapeData();
       res.json(scrapeData);
       await page.browser().close();
+      console.log("data has been fetched");
   } catch (error) {
       res.status(500).json({ error: "An error occurred while fetching data." });
   }
