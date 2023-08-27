490f0c05817d85c90143acc32202adf4b3c42bc6
brickphone <mattis.hutt@gmail.com>
2023-08-27 15:41:46 +0200

    test

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const scrapeData = require("./Scraper/scraper");
-const mongoose = require("mongoose");
 const routes = require("./routes/routes");
 
-const mongoString = process.env.DATABASE_URL
+const mongoString = p<rocess.env.DATABASE_URL
 mongoose.connect(mongoString);
 const database = mongoose.connection
 
@@ -24,7 +23,6 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-<<<<<<< HEAD
 app.use(express.json());
 
 app.get("/status", (request, response) => {
@@ -37,11 +35,6 @@ app.get("/status", (request, response) => {
 
 
 app.get("/api/data", async(req, res) => {
-=======
-app.use('/api', routes);
-
-router.get("/api/data", async(req, res) => {
->>>>>>> 44b4076241d843d3e6cb8f9d68eb6842153f98ee
   try {
     const page = await openPage();
     const dataScraped = await scrapeData();
